e235987a01c8ee7149176c1642bffadf2a40f3a8
Abhi Pillai <abhi.pillai1@gmail.com>
2016-11-02 17:50:26 -0400

    Remove redis comments

--- server.go
+++ server.go
@@ -5,19 +5,11 @@ import (
   "github.com/gin-gonic/gin"
   "github.com/olahol/melody"
   "net/http"
-//  "github.com/gorilla/websocket"
-//  "github.com/garyburd/redigo/redis"
-//  "github.com/soveran/redisurl"
   "os"
   "database/sql"
   _ "github.com/go-sql-driver/mysql"
   "time"
 )
-
-type message struct {
-  Handle string `json:"handle"`
-  Text   string `json:"text"`
-}
 
 func main() {
   r := gin.Default()
@@ -25,14 +17,6 @@ func main() {
 
   db_url := os.Getenv("SHAREDCHAT_DB_URL")
   db, _ := sql.Open("mysql", db_url)
-
-  //// Connect using os.Getenv("REDIS_URL").
-  // conn, err := redisurl.Connect()
-  // if err != nil {
-  //   fmt.Println(err)
-  //   os.Exit(1)
-  // }
-  // defer conn.Close()
 
   r.GET("/", func(c *gin.Context) {
     http.ServeFile(c.Writer, c.Request, "./index.html")
@@ -53,6 +37,7 @@ func main() {
       }
       allMessages = append(allMessages, content)
     }
+
     c.JSON(200, gin.H{
       "messages": allMessages,
     })
